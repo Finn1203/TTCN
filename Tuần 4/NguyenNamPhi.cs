7d83758a3e37df2c6ca557156636b064f44706cf
NVQuyet202 <130235015+NVQuyet202@users.noreply.github.com>
2023-04-09 09:33:15 +0700

    Add files via upload

---
+++ Tuần 4/NguyenNamPhi.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+namespace VietTutsCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            Console.Write("Cac so nguyen to nho hon 100 la: \n");
+            for (i = 0; i < 100; i++)
+            {
+                if (isPrimeNumber(i))
+                {
+                    Console.Write("{0} ", i);
+                }
+            }
+
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+
+
+   
+        static Boolean isPrimeNumber(int n)
+        {
+            // so nguyen n < 2 khong phai la so nguyen to
+            if (n < 2)
+            {
+                return false;
+            }
+            // check so nguyen to khi n >= 2
+            int squareRoot = (int)Math.Sqrt(n);
+            int i;
+            for (i = 2; i <= squareRoot; i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Tuần 4/NguyenVanToan.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] myArray = { "apple", "banana", "orange", "pear", "kiwi" };
+
+            Array.Sort(myArray);
+
+            foreach (string fruit in myArray)
+            {
+                Console.WriteLine(fruit);
+            }
+
+            Array.Reverse(myArray);
+
+            foreach (string fruit in myArray)
+            {
+                Console.WriteLine(fruit);
+            }
+        }
+    }
+}
