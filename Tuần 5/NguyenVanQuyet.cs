46d03555b8faee8f30c2e0706a003ad4bb6f0381
NVQuyet202 <130235015+NVQuyet202@users.noreply.github.com>
2023-04-14 22:46:55 +0700

    Add files via upload

---
+++ Tuần 5/NguyenVanQuyet.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace VietTutsCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Nhap so nguyen duong n = ");
+            n = int.Parse(Console.ReadLine());
+            // phan tich so nguyen duong n
+            phanTichSoNguyen(n);
+
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+
+        /**
+       * Phan tich so nguyen n thanh tich cac thua so nguyen to
+       */
+        static void phanTichSoNguyen(int n)
+        {
+            int i = 2;
+            int dem = 0;
+            int[] a = new int[100];
+            int originalN = n;
+            // phan tich
+            while (n > 1)
+            {
+                if (n % i == 0)
+                {
+                    n = n / i;
+                    a[dem++] = i;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            // neu dem = 0 thi n la nguyen to
+            if (dem == 0)
+            {
+                a[dem++] = n;
+            }
+            // in ket qua ra man hinh
+            Console.Write("{0} = ", originalN);
+            for (i = 0; i < dem - 1; i++)
+            {
+                Console.Write("{0} x ", a[i]);
+            }
+            Console.Write("{0}", a[dem - 1]);
+        }
+    }
+}
