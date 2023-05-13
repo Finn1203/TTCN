f05b7987df7a7a576a48ee3c1752e398a18083a2
Phạm Đức Minh <102680918+youngboizseetinhh@users.noreply.github.com>
2023-05-13 20:40:38 +0700

    Add files via upload

---
+++ Tuần 7/phamducminh (2).cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class TowerOfHanoi
+{
+    static void Main(string[] args)
+    {
+        int n = 3; // Số đĩa
+        int from = 1, to = 3, temp = 2; // Số cột
+
+        Move(n, from, to, temp);
+    }
+
+    static void Move(int n, int from, int to, int temp)
+    {
+        if (n == 1)
+        {
+            Console.WriteLine($"Move disk 1 from column {from} to column {to}");
+            return;
+        }
+
+        Move(n - 1, from, temp, to);
+        Console.WriteLine($"Move disk {n} from column {from} to column {to}");
+        Move(n - 1, temp, to, from);
+    }
+}
+
